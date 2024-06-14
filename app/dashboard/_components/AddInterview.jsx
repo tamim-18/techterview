524d768816a0f2b391f7a8073c9e1271caace4e2
tamim-18 <tamim11903060@gmail.com>
2024-06-14 23:38:10 +0600

    add new features

--- app/dashboard/_components/AddInterview.jsx
+++ app/dashboard/_components/AddInterview.jsx
@@ -11,9 +11,17 @@ import {
 import { Button } from "@/components/ui/button";
 import { Ghost } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const AddInterview = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [jobTitle, setJobTitle] = React.useState();
+  const [jobDescription, setJobDescription] = React.useState();
+  const [experience, setExperience] = React.useState();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(jobTitle, jobDescription, experience);
+  };
   return (
     <div className="">
       <div
@@ -31,24 +39,62 @@ const AddInterview = () => {
               Tell us more about your job interviewing
             </DialogTitle>
             <DialogDescription>
-              <div className=" ">
-                <h2 className="">Tell me about your profile in more details</h2>
-              </div>
-              <div className=" mt-7 my-2">
-                <label
-                  htmlFor="title"
-                  className="block text-sm font-medium mb-2"
-                >
-                  What are you looking for in a new position?
-                </label>
-                <Input placeholder="eg. ML engineers" />
-              </div>
-              <div className=" flex justify-end gap-2">
-                <Button>Start InterView</Button>
-                <Button onClick={() => setIsOpen(false)} variant={Ghost}>
-                  Cancel
-                </Button>
-              </div>
+              <form action="" onSubmit={onSubmit}>
+                <div className=" ">
+                  <h2 className="">
+                    Tell me about your profile in more details
+                  </h2>
+                </div>
+                <div className=" mt-7 my-2">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium mb-2"
+                  >
+                    What are you looking for in a new position?
+                  </label>
+                  <Input
+                    placeholder="eg. ML engineers"
+                    required
+                    onChange={(e) => setJobTitle(e.target.value)}
+                  />
+                </div>
+                <div className=" mt-7 my-2">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium mb-2"
+                  >
+                    Job Description/Stack (in details)
+                  </label>
+                  <Textarea
+                    placeholder="eg. ML,DL,LLM,NLP etc.."
+                    required
+                    onChange={(e) => setJobDescription(e.target.value)}
+                  />
+                </div>
+                <div className=" mt-7 my-2">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium mb-2"
+                  >
+                    Enter Your Experience Year
+                  </label>
+                  <Input
+                    placeholder="eg 4 years "
+                    type="number"
+                    onChange={(e) => setExperience(e.target.value)}
+                  />
+                </div>
+                <div className=" flex justify-end gap-2">
+                  <Button type="submit"> Start InterView</Button>
+                  <Button
+                    type="button"
+                    onClick={() => setIsOpen(false)}
+                    variant={Ghost}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </form>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
