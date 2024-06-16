b0cacb56ca13385a54c851ef2f94b8a2b038e0f1
tamim-18 <tamim11903060@gmail.com>
2024-06-17 01:18:42 +0600

    eid mubarak

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -3,9 +3,13 @@ import { db } from "@/utils/db";
 import { Interview } from "@/utils/schema";
 import React from "react";
 import { eq } from "drizzle-orm";
+import { WebcamIcon } from "lucide-react";
+import Webcam from "react-webcam";
+import { Button } from "@/components/ui/button";
 
 const InterViewPage = ({ params }) => {
   const [interview, setInterview] = React.useState();
+  const [webcamEnabled, setWebcamEnabled] = React.useState(false);
   React.useEffect(() => {
     console.log("Params: ", params);
     getInterview();
@@ -15,18 +19,49 @@ const InterViewPage = ({ params }) => {
     const interviewId = params.interviewId;
     // console.log(params.interviewId === Interview.mockInterviewId);
     // console.log("InterviewId: ", interviewId);
+
+    // Fetch the interview from the database
     try {
       const result = await db
         .select()
         .from(Interview)
         .where(eq(Interview.mockInterviewId, params.interviewId));
-      console.log(result);
+      //console.log(result);
       setInterview(result[0]);
     } catch (error) {
       console.error("Error fetching interview data: ", error);
     }
   };
-  return <div>InterViewPage</div>;
+  return (
+    <div className="my-10 flex flex-col items-center">
+      <h2 className="font-bold text-3xl mb-6 text-center">Let's Get Started</h2>
+      <div className="w-full max-w-lg">
+        {webcamEnabled ? (
+          <Webcam
+            onUserMedia={() => setWebcamEnabled(true)}
+            onUserMediaError={() => setWebcamEnabled(false)}
+            style={{
+              height: 300,
+              width: "100%",
+              borderRadius: "0.5rem",
+              objectFit: "cover",
+            }}
+            className="shadow-lg"
+          />
+        ) : (
+          <div className="flex flex-col items-center">
+            <WebcamIcon className="w-full h-72 bg-secondary my-7 border-lg p-20" />
+            <Button
+              onClick={() => setWebcamEnabled(true)}
+              className="mt-4 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600"
+            >
+              Enable webcam and microphone
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default InterViewPage;
